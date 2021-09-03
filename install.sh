0f2516864f484d56cb2baf3f30f1171ae5594062
Colton <colathro@microsoft.com>
2021-09-03 14:27:00 -0700

    add back sudo

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # system setup
-raspi-config nonint do_hostname gormin
+sudo raspi-config nonint do_hostname gormin
 
 # gps dependencies
-apt-get install gpsd gpsd-clients
+sudo apt-get install gpsd gpsd-clients
 
 # gps settings
-raspi-config nonint do_serial 1
+sudo raspi-config nonint do_serial 1
 
 # screen dependencies
-apt install -y python3-dev
-apt install -y python-smbus i2c-tools
-apt install -y python3-pil
-apt install -y python3-pip
-apt install -y python3-setuptools
-apt install -y python3-rpi.gpio
+sudo apt install -y python3-dev
+sudo apt install -y python-smbus i2c-tools
+sudo apt install -y python3-pil
+sudo apt install -y python3-pip
+sudo apt install -y python3-setuptools
+sudo apt install -y python3-rpi.gpio
 
 # screen settings
-raspi-config nonint do_i2c 1
+sudo raspi-config nonint do_i2c 1
 
