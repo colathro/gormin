12a1a00543e9faa53543afdb0cd1f48f965a22c2
Colton <colathro@microsoft.com>
2021-09-03 15:57:15 -0700

    gps to test

--- test/serial_gps.py
+++ test/serial_gps.py
@@ -1,18 +1,14 @@
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
-import time
-
 import serial
 
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
 SERIAL_PORT = "/dev/serial0"
+running = True
 
-
+# In the NMEA message, the position gets transmitted as:
+# DDMM.MMMMM, where DD denotes the degrees and MM.MMMMM denotes
+# the minutes. However, I want to convert this format to the following:
+# DD.MMMM. This method converts a transmitted string to the desired format
 def formatDegreesMinutes(coordinates, digits):
-
+    
     parts = coordinates.split(".")
 
     if (len(parts) != 2):
@@ -20,7 +16,7 @@ def formatDegreesMinutes(coordinates, digits):
 
     if (digits > 3 or digits < 2):
         return coordinates
-
+    
     left = parts[0]
     right = parts[1]
     degrees = str(left[:digits])
@@ -28,7 +24,9 @@ def formatDegreesMinutes(coordinates, digits):
 
     return degrees + "." + minutes
 
-
+# This method reads the data from the serial port, the GPS dongle is attached to,
+# and then parses the NMEA messages it transmits.
+# gps is the serial port, that's used to communicate with the GPS adapter
 def getPositionData(gps):
     data = gps.readline()
     message = data[0:6]
@@ -38,54 +36,28 @@ def getPositionData(gps):
         parts = data.split(",")
         if parts[2] == 'V':
             # V = Warning, most likely, there are no satellites in view...
-            return ("syncing", "syncing")
+            print "GPS receiver warning"
         else:
             # Get the position data that was transmitted with the GPRMC message
             # In this example, I'm only interested in the longitude and latitude
             # for other values, that can be read, refer to: http://aprs.gids.nl/nmea/#rmc
             longitude = formatDegreesMinutes(parts[5], 3)
             latitude = formatDegreesMinutes(parts[3], 2)
-            return (str(latitude), str(longitude))
+            print "Your position: lon = " + str(longitude) + ", lat = " + str(latitude)
     else:
         # Handle other NMEA messages and unsupported strings
         pass
-    return ("pass data", "pass data")
 
+print "Application started!"
+gps = serial.Serial(SERIAL_PORT, baudrate = 9600, timeout = 0.5)
 
-RST = None
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-
-disp.begin()
-disp.clear()
-disp.display()
-
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
-
-draw = ImageDraw.Draw(image)
-
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-padding = -2
-top = padding
-bottom = height-padding
-
-x = 0
-
-font = ImageFont.load_default()
-
-gps = serial.Serial(SERIAL_PORT, baudrate=9600, timeout=0.5)
-
-while True:
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-    lat, lon = getPositionData(gps)
-    draw.text((x, top), f"la:{lat}",  font=font, fill=255)
-    draw.text((x, top+8), f"lo:{lon}", font=font, fill=255)
-    draw.text((x, top+16), "little",  font=font, fill=255)
-    draw.text((x, top+25), "baka",  font=font, fill=255)
-
-    disp.image(image)
-    disp.display()
-    time.sleep(.5)
+while running:
+    try:
+        getPositionData(gps)
+    except KeyboardInterrupt:
+        running = False
+        gps.close()
+        print "Application closed!"
+    except:
+        # You should do some error handling here...
+        print "Application error!"
