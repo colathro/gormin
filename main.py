f3a509199fec7bd7e8314740bfc1d7094638d134
Colton <colathro@microsoft.com>
2021-09-03 18:42:20 -0700

    log to file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from location import Location
 from screen import Screen
 
+import time
+import datetime
+
 if __name__ == "__main__":
     s = Screen()
     l = Location()
@@ -8,3 +11,6 @@ if __name__ == "__main__":
     while True:
         data = l.get_latitude_longitude()
         s.render("Gormin G420", "la:" + data[0], "lon:" + data[1], "Running")
+        with open("data.csv", "a") as f:
+            f.write(f"{datetime.datetime.now()},{data[0]},{data[1]}")
+        time.sleep(1)
