252c38d98e937946300ef8e7314f31d30f9dc19e
Colton <colathro@microsoft.com>
2021-09-04 14:01:24 -0700

    button await

--- main.py
+++ main.py
@@ -3,14 +3,38 @@ from screen import Screen
 
 import time
 import datetime
+import threading
+from gpiozero import Button
+
+
+class State:
+
+    def __init__(self):
+        self.running = False
+        self.button = Button(17)
+        thread = threading.Thread(target=self.await_pause, args=())
+        thread.daemon = True
+        thread.start()
+
+    def await_pause(self):
+        while True:
+            self.button.wait_for_press()
+            self.running = not self.running
+
 
 if __name__ == "__main__":
     s = Screen()
     l = Location()
+    state = State()
 
     while True:
-        data = l.get_latitude_longitude()
-        s.render("Gormin G420", "la:" + data[0], "lon:" + data[1], "Running")
-        with open("data.csv", "a") as f:
-            f.write(f"{datetime.datetime.now()},{data[0]},{data[1]}\n\r")
+        if state.running:
+            data = l.get_latitude_longitude()
+            s.render("Gormin 420", "la:" +
+                     data[0], "lon:" + data[1], "Running")
+            with open("data.csv", "a") as f:
+                f.write(f"{datetime.datetime.now()},{data[0]},{data[1]}\n\r")
+        else:
+            s.render("Gormin 420", "la:" +
+                     data[0], "lon:" + data[1], "Stopped")
         time.sleep(1)
